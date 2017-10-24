99484ab78d6d152c6e355c44a9d9556cb626a3b6
Michael McGee <dineromike@yahoo.com>
2017-10-23 21:39:41 -0500

    added more code to prevent bad entry in numeric input

--- src/app/directives/numeric-input.directive.ts
+++ src/app/directives/numeric-input.directive.ts
@@ -1,36 +1,70 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input } from '@angular/core';
 
 @Directive({
     selector: '[numeric-input]',
-    host: { 
+    host: {
         '(keypress)': 'checkInput($event)',
+        '(keyup)': 'keyUp($event)',
+        '(keydown)': 'keyDown($event)',
         '(paste)': 'paste($event)',
-        }
+    }
 })
 export class NumericInputDirective implements OnInit {
 
-    constructor(private el: ElementRef) {
-        console.log('constructor');
-    }
+    @Input() max: number;
+    @Input() decimals: number;
+    keyDownValue = '';
+
+    constructor(private el: ElementRef) { }
 
     ngOnInit(): void {
-        console.log('ngOnInit');
+
+        if (isNaN(this.decimals)) {
+            this.decimals = 0
+        }
+        else {
+            this.decimals = Math.abs(Math.floor(this.decimals));
+        }
     }
 
-    paste = (event:Event):void => {
-        console.log('paste event');
+    paste = (event: Event): void => {
+
         event.preventDefault();
-        
     }
+
+    keyUp = (event: KeyboardEvent): void => {
+
+        if (!isNaN(this.max)) {
+            if (!isNaN(this.el.nativeElement.value)) {
+                var num = Number(this.el.nativeElement.value)
+
+                if (num > this.max) {
+                    this.el.nativeElement.value = this.keyDownValue;
+                }
+            }
+        }
+    };
+
+    keyDown = (event: KeyboardEvent): void => {
+        this.keyDownValue = this.el.nativeElement.value;
+        // console.log('value length', this.el.nativeElement.value.length)
+        // console.log('selection start', this.el.nativeElement.selectionStart)
+    };
+
     checkInput = (event: KeyboardEvent): void => {
-        console.clear()
-        console.log(event);
-        console.log(this.el.nativeElement.value)
+
         var value: string = this.el.nativeElement.value;
 
         let inputChar = String.fromCharCode(event.charCode);
+        // TODO you were allowing negatived you don't need know, so removed
+        // const pattern = /[0-9\.\-\ ]/;
 
-        const pattern = /[0-9\.\-\ ]/;
+        var pattern = /[0-9\.\ ]/;
+        if (this.decimals === 0) {
+            pattern = /[0-9\ ]/;
+        }
+
+
         if (!pattern.test(inputChar)) {
             // invalid character, prevent input
             event.preventDefault();
@@ -41,6 +75,6 @@ export class NumericInputDirective implements OnInit {
         if (value.indexOf('.') >= 0 && event.key === '.') {
             event.preventDefault();
             return;
-        }        
-    }
+        }
+    };
 }
