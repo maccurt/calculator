8b82feb186938af05754113114c08450fcba03df
Michael McGee <dineromike@yahoo.com>
2017-10-25 18:01:15 -0500

    added code coverage for build test

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['cobertura', 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
