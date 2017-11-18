143a0899845eb0b7b571a6af422fe94cc8347e8d
michaelmcgee <dineromike@yahoo.com>
2017-11-18 05:26:29 -0600

    removed pre-calculation

--- src/app/future-value/future-value.component.ts
+++ src/app/future-value/future-value.component.ts
@@ -28,10 +28,10 @@ export class FutureValueComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.monthlyPayment = 50;
-    this.ratePercent = 8.5;
-    this.years = 20;
-    this.calculate();
+    // this.monthlyPayment = 50;
+    // this.ratePercent = 8.5;
+    // this.years = 20;
+    // this.calculate();
   }
 
   calculate = () => {
